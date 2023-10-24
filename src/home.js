6493febca063d7d191412aeeec93ca3bc81e89df
Quan Kien <qtkk20a@gmail.com>
2023-10-24 23:19:46 +0700

    front

---
+++ src/home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+import Background from './background';
+import Btn from './buton';
+import { darkGreen, green } from './Constants';
+
+const Home = (props) => {
+  return (
+    <Background>
+      <View style={{ marginHorizontal: 40, marginVertical: 100 }}>
+      <Text style={{ color: 'white', fontSize: 50 }}>Let's</Text>
+      <Text style={{ color: 'white', fontSize: 50, marginBottom: 40 }}>DoctorOnline</Text>
+      <Btn bgColor={green} textColor='white' btnLabel="Login" Press={() => props.navigation.navigate("Login")} />
+      <Btn bgColor='white' textColor={darkGreen} btnLabel="Signup" Press={() => props.navigation.navigate("Signup")} />
+      </View>
+    </Background>
+  );
+}
+
+const styles = StyleSheet.create({})
+
+export default Home;
